148ed469df6295c8c4ac086b009404082b15ead3
Markus Klemm <markus@markusklemm.net>
2016-06-24 15:40:32 +0200

    Added a sophisticated win32 error handling class

--- src/winfs/windows_error.cpp
+++ src/winfs/windows_error.cpp
@@ -1,26 +1,102 @@
 #include "windows_error.h"
 
 #include <windows.h>
+#include "nfs/nfs3.h"
 
 namespace windows{
 
-win32_exception::win32_exception(std::string msg, std::uint32_t get_last_error_code) :
-    std::runtime_error(""), final_msg(msg), error_code(get_last_error_code)
-{
+namespace{
+std::string& win32_code_to_text(win32_error_code_t error_code, std::string &string){
     LPSTR tmp = NULL;
     auto format_message_return = FormatMessageA(
                 FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS |FORMAT_MESSAGE_ALLOCATE_BUFFER,
                 NULL,
-                this->error_code,
+                error_code,
                 MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
                 (LPSTR)&tmp,
                 0,
                 NULL
                 );
-    this->final_msg += ": Code: \"" +
-            std::to_string(this->error_code) + "\""
+    if (format_message_return == 0){
+        throw std::runtime_error("FormatMessageA failed in win32_code_to_text");
+    }
+    string += ": Code: \"" +
+            std::to_string(error_code) + "\""
             + " = \"" + std::string(tmp) + "\"";
     LocalFree(tmp);
+    return string;
+}
+}
+
+win32_return_t::win32_return_t (std::function<bool (void)> win32_call)
+{
+    this->first = win32_call();
+    if (!first){
+        this->second = GetLastError();
+    }
+}
+
+
+win32_exception::win32_exception(std::string msg, std::uint32_t get_last_error_code) :
+    std::runtime_error(""), final_msg(msg), error_code(get_last_error_code)
+{
+    win32_code_to_text(this->error_code, this->final_msg);
+}
+
+std::string win32_return_t::to_string() const{
+    std::string msg("Function returned ");
+    msg += (first ? "successfull" : " unsucessfull");
+
+    if (!first){
+        msg += " and set GetLastError to" ;
+        win32_code_to_text(second,msg);
+    }
+    return msg;
+}
+
+uint32_t to_nfs3_error(win32_error_code_t win32_error){
+    nfs3::status_t nfs_error;
+    switch (win32_error){
+    case ERROR_SUCCESS:
+        nfs_error = nfs3::status_t::OK;
+        break;
+    case ERROR_FILE_NOT_FOUND:
+    case ERROR_PATH_NOT_FOUND:
+        nfs_error = nfs3::status_t::ERR_NO_ENTRY;
+        break;
+    case ERROR_TOO_MANY_OPEN_FILES:
+    case ERROR_WRITE_FAULT:
+    case ERROR_READ_FAULT:
+        nfs_error = nfs3::status_t::ERR_IO;
+        break;
+    case ERROR_INVALID_HANDLE:
+        nfs_error = nfs3::status_t::ERR_STALE;
+        break;
+    case ERROR_NOT_ENOUGH_MEMORY:
+    case ERROR_OUTOFMEMORY:
+    case ERROR_HANDLE_DISK_FULL:
+    case ERROR_DISK_FULL:
+        nfs_error = nfs3::status_t::ERR_NOSPC;
+       break;
+    case ERROR_INVALID_DRIVE:
+    case ERROR_DEV_NOT_EXIST:
+        nfs_error = nfs3::status_t::ERR_NODEV;
+        break;
+    case ERROR_FILE_EXISTS:
+    case ERROR_ALREADY_EXISTS:
+        nfs_error = nfs3::status_t::ERR_EXIST;
+        break;
+    case ERROR_ACCESS_DENIED:
+        nfs_error = nfs3::status_t::ERR_PERM;
+        break;
+    case ERROR_INVALID_ACCESS:
+        nfs_error = nfs3::status_t::ERR_ACCESS;
+        break;
+    default:
+        nfs_error = nfs3::status_t::ERR_SERVERFAULT;
+        break;
+    }
+    return static_cast<uint32_t> (nfs_error);
 }
 
 }
--- src/winfs/windows_error.h
+++ src/winfs/windows_error.h
@@ -8,11 +8,14 @@
 #include <exception>
 #include <string>
 #include <utility>
+#include <functional>
 
 namespace windows{
 using win32_error_code_t = uint32_t; //*< Aka DWORD
 
-/*! @brief Exception class for the windows API aka WIN32 functions which are using GetLastError*/
+/*! @brief Exception class for the windows API aka WIN32 functions which are using GetLastError
+ * @notice Handles the inconsistent behaveiour of successfull but error setting calls
+*/
 class win32_exception : public std::runtime_error{
 public:
     /*! @param msg User supplied message which will lead the full exception text
@@ -34,11 +37,57 @@ private:
     win32_error_code_t error_code;
 };
 
-struct win32_return_t : std::pair<bool, win32_error_code_t> {
-    win32_return_t (bool func_return);
+/*! @brief Utility class to pass the complete result of a win32 call
+ * @todo When C++17 usable change super class to public std::pair<bool, std::optional> and exception in get_error to std::bad_optional_access
+ * */
+class win32_return_t : private std::pair<bool, win32_error_code_t> {
+public:
+    /*! @brief to manually inidcate a successfull call
+     * */
+    constexpr win32_return_t () : std::pair<bool, win32_error_code_t> (true,0) {}
+
     win32_return_t (bool func_return, win32_error_code_t get_last_error_return);
+    /*! @brief Will execute the given win32_call, and store the results accordingly
+     * @attention Will not store an error if the win32 call returned true
+     * @param win32_call Callback to a win32 bool returning function, which will
+     * return false if there is an error which will be retrived by GetLastError
+     * */
+    win32_return_t (std::function<bool (void)> win32_call);
+
+   /*! @brief Will execute the given win32 call, and store a GetLastError result if it didn't returned the excepted value
+    * @param win32_call Callback to a win32 which should return @ref expected
+    * @param expected Expected value from win32 routine
+    * */
+
+    /*! If the return value of the given win32_routine does not match the criteria (per default is equal), get error accordingly
+     * */
+    template <typename ReturnType, typename Comp = std::equal_to>
+    win32_return_t (std::function<ReturnType (void)> win32_call, ReturnType expected)
+        : win32_return_t(Comp(win32_call(),expected))
+    {
+    }
+
+    /*! @brief To determine whatever win32 call was successfull or not
+     * */
+    explicit operator bool() const{
+        return this->first;
+    }
+    bool operator!() const{
+        return !this->first;
+    }
+    /*! @brief Returns the error code which was retrieved after the win32 call or throws an exception if no error was set
+     * @throws std::logic_error
+     * */
+    win32_error_code_t get_error() const{
+        if (first){
+            throw std::runtime_error("No error was set");
+        }
+        return this->second;
+    }
+    std::string to_string() const;
 
 };
 
+uint32_t to_nfs3_error(win32_error_code_t win32_error);
 }
 
